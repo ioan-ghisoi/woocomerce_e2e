faa713daae503c54daff329f389b31f6df37dd59
Ioan Ghisoi <ionutghisoi95@gmail.com>
2018-01-16 16:29:26 +0000

    localhost

--- tests/features/step_definitions/setup.spec.js
+++ tests/features/step_definitions/setup.spec.js
@@ -12,7 +12,7 @@ export default function () {
       width: VAL.resolution_w,
       height: VAL.resolution_h,
     }, true);
-    browser.url('http://127.0.0.1/');
+    browser.url('http://localhost/');
     browser.click("#asd");
   });
   this.Given(/^I go to the backend of Checkout's plugin$/, () => {
