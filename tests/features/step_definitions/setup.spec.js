10870849e2c07476736ec70a96dab31ce0410fde
Ioan Ghisoi <ionutghisoi95@gmail.com>
2018-01-17 09:53:31 +0000

    Run test

--- tests/features/step_definitions/setup.spec.js
+++ tests/features/step_definitions/setup.spec.js
@@ -12,8 +12,6 @@ export default function () {
       width: VAL.resolution_w,
       height: VAL.resolution_h,
     }, true);
-    browser.url('http://127.0.0.1/wordpress');
-    browser.click("#asd");
   });
   this.Given(/^I go to the backend of Checkout's plugin$/, () => {
     browser.url(URL.wordpress_base + URL.admin_path);
